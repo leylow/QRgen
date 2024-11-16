b89c79335d6b6fb0c07328d503edd7cee1e0247d
leylow <kalesanwoyinka99@gmail.com>
2024-11-16 07:33:45 +0100

    new changes

--- QR-Code Generator/vite.config.js
+++ QR-Code Generator/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import ghPages from 'vite-plugin-gh-pages';
+//import ghPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   base: '/QRgen/',
 })
